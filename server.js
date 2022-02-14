ca1a77b729c6f7eee409ce4082825a6c37aba641
Roger Zhang <1787009521@qq.com>
2022-02-14 18:59:09 +0800

    bug: node静态服务器

--- server.js
+++ server.js
@@ -1,17 +1,40 @@
 const express = require('express')
 const path = require('path')
 const fs = require('fs')
+const qiniu = require('qiniu')
 
 const app = express()
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'src', 'index.html'))
+app.use('/', express.static(__dirname, 'src'))
+app.get('/', function (req, res) {
+  res.sendFile('/src/index.html', {
+    root: __dirname
+  })
 })
 
-let config = fs.readFileSync('./qiniu-key.json')
-config = JSON.parse(config) // buffer格式 需要转换
-console.log(config)
+app.get('/uploadToken', function (req, res) {
+  res.json({uploadToken: getToken()})
+})
+
 
 app.listen('3000', function () {
   console.log('Server is running...')
 })
+
+//-------------------Function-------------------//
+
+function getToken() {
+  let config = fs.readFileSync('./qiniu-key.json')
+  config = JSON.parse(config) // buffer格式 需要转换
+  let {accessKey, secretKey} = config
+  let mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+  let options = {
+    scope: 'music-163s',
+  };
+  let putPolicy = new qiniu.rs.PutPolicy(options);
+// 每次启动的uploadToken都是一样的.
+  let uploadToken = putPolicy.uploadToken(mac);
+
+  console.log(uploadToken)
+  return uploadToken
+}
