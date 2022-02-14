7a80db48d03915e329348780492cf9973e3d02a2
Roger Zhang <1787009521@qq.com>
2022-02-14 18:23:58 +0800

    feat: 读取文件获取ak

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const path = require('path')
+const fs = require('fs')
 
 const app = express()
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'src', 'index.html'))
 })
+
+let config = fs.readFileSync('./qiniu-key.json')
+config = JSON.parse(config) // buffer格式 需要转换
+console.log(config)
 
 app.listen('3000', function () {
   console.log('Server is running...')
