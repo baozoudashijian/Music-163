03612e0a43ce1814ffc0e0cb452f1af8b7381650
Roger Zhang <1787009521@qq.com>
2022-02-20 15:09:04 +0800

    feat: 为了部署到heroku

---
+++ leancloud.js
@@ -0,0 +1,9 @@
+window.appId = 'jXow4jGHN652DLfE6c3rwRSp-gzGzoHsz',
+window.appKey = 'jmG07sfWYrP5p6wI9md9Cmk9'
+window.serverURL = 'https://jxow4jgh.lc-cn-n1-shared.com'
+
+AV.init({
+    appId: window.appId,
+    appKey: window.appKey,
+    serverURL: window.serverURL
+});
