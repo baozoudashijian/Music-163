9f91327b4077e74bf1799c242659bd5bc9e4cc17
Roger Zhang <1787009521@qq.com>
2022-03-06 21:14:36 +0800

    feat: play页面样式

--- src/web/js/song.js
+++ src/web/js/song.js
@@ -17,7 +17,17 @@ const PlayHeader = React.createClass({
     render() {
         return (
             <div className="ph-container">
-
+                <div className="disk">
+                    <div className="song-return">
+                        <div className="wrapper">
+                            <div className="img">
+                                <img className="u-img" alt="song-img" src="https://p1.music.126.net/8IEucUOp6E1AJfDb5RrPNw==/109951167104850464.jpg?imageView&thumbnail=360y360&quality=75&tostatic=0" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="song-plybtn"></div>
+                </div>
+                <div className="lrc"></div>
             </div>
         )
     }
@@ -25,7 +35,14 @@ const PlayHeader = React.createClass({
 const PlayBody = React.createClass({
     render() {
         return (
-            <div>body</div>
+            <div className="pb-container">
+                <div className="wrapper">
+                    <div className="open-app">
+                        <button>打开</button>
+                    </div>
+                    <span className="dl">下载</span>
+                </div>
+            </div>
         )
     }
 })
@@ -37,7 +54,9 @@ const PlayMusic = React.createClass({
                 <div className="pm-container">
                 
                 </div>
-                <div className="top-shade"></div>
+                <div className="top-shade">
+                    <div className="ply-pointer"></div>
+                </div>
                 <div className="bottom-shade"></div>
                 <PlayHeader />
                 <PlayBody />
