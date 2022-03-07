565315829383917d278d10d3a8769bfb9df8e9ed
Roger Zhang <1787009521@qq.com>
2022-03-07 23:18:03 +0800

    feat: 添加播放暂停动画

--- src/web/js/song.js
+++ src/web/js/song.js
@@ -17,25 +17,32 @@ const PlayHeader = React.createClass({
     getInitialState() {
         return { playState: false };
     },
+    getDefaultProps() {
+        return {
+            catchState: () => {}
+        }
+    },
     toPlay() {
         console.log('播放音乐')
         console.log(this.refs.music)
         this.refs.music.play()
         this.setState({ playState: true })
+        this.props.catchState(true)
     },
     toPause() {
         console.log('暂停音乐')
         this.refs.music.pause()
         this.setState({ playState: false })
+        this.props.catchState(false)
     },
     render() {
         const { playState } = this.state
         return (
             <div className="ph-container">
                 <div className="disk">
-                    <div className="song-return" onClick={this.toPause}>
+                    <div className='song-return' onClick={this.toPause}>
                         <div className="wrapper">
-                            <div className="img">
+                            <div className={['img', playState ? 'active':''].join(' ')}>
                                 <img className="u-img" alt="song-img" src="https://p1.music.126.net/8IEucUOp6E1AJfDb5RrPNw==/109951167104850464.jpg?imageView&thumbnail=360y360&quality=75&tostatic=0" />
                                 <div className="u-audio">
                                     <audio ref="music" controls src="https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3"></audio>
@@ -72,17 +79,24 @@ const PlayBody = React.createClass({
 })
 
 const PlayMusic = React.createClass({
+    getInitialState() {
+        return { playState: false };
+    },
+    catchState(state) {
+        this.setState({playState: state})
+    },
     render() {
+        const { playState } = this.state
         return (
             <div>
                 <div className="pm-container">
 
                 </div>
                 <div className="top-shade">
-                    <div className="ply-pointer"></div>
+                    <div className={['ply-pointer', playState ? 'active':''].join(' ')}></div>
                 </div>
                 <div className="bottom-shade"></div>
-                <PlayHeader />
+                <PlayHeader catchState={this.catchState} />
                 <PlayBody />
             </div>
 
